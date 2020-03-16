fb7a30f5baab915e010f254a5e25ba683520f84b
tiators <61823690+tiators@users.noreply.github.com>
2020-03-16 09:02:44 +0800

    调整格式

--- diy.sh
+++ diy.sh
@@ -5,5 +5,6 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
